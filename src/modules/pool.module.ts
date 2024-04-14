277cda8e43baa3abe83b4a71fc1ea240fee918c3
Mia Fallon <pop37corn@gmail.com>
2024-04-14 15:18:32 -0400

    Add custom option dbEnvKey, defaults to currently used value.

--- src/modules/pool.module.ts
+++ src/modules/pool.module.ts
@@ -2,6 +2,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import pkg from 'pg';
+import config from '../config';
+
+const { dbEnvKey } = config();
 
 const { Pool } = pkg;
-export default new Pool({ connectionString: process.env.PG_DATABASE_URL });
+
+export default new Pool({ connectionString: process.env[dbEnvKey] });
